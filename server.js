4b5425cbc178b41980c52d2dc0b004f48ba9b186
SnowArrow <ohm2549@hotmail.com>
2025-03-24 09:50:09 +0700

    Fix Vercel

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const helmet = require('helmet');
 const {xss} = require('express-xss-sanitizer');
 const rateLimit=require('express-rate-limit');
 const hpp=require('hpp');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require ('swagger-ui-express');
+const bodyParser = require('body-parser');
 const cors=require('cors');
 
 //Load env vars
@@ -39,8 +42,16 @@ app.use(hpp());
 //Enable CORS
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 //Cookie parser
 app.use(cookieParser());
+
+const limiter=rateLimit({
+    windowsMs:10*60*1000,//10 mins
+    max: 50
+});
+app.use(limiter);
 
 //Mount routers
 app.use('/api/v1/companies', companies);
@@ -50,13 +61,33 @@ app.use('/api/v1/interviews',interviews);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, 
+    console.log(
+        "Server running in",
+        process.env.NODE_ENV,
+        "on" + `${process.env.HOST} mode on port ${PORT}`)
+    );
 
-const limiter=rateLimit({
-    windowsMs:10*60*1000,//10 mins
-    max: 50
-});
-app.use(limiter);
+const swaggerOptions ={
+    swaggerDefinition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Library API',
+            version:'1.0.0',
+            description:'Online Job Fair API'
+        },
+        servers:[
+            {
+                url: process.env.HOST + ':' + PORT + '/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));  
 
 process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error: ${err.message}`);
